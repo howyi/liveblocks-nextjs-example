4bc7367996484c7b708f0d49f849d11818e80882
t-hayashi <t.hayashi.dev@gmail.com>
2023-12-09 15:35:48 +0900

    add Editor Collabration

--- app/Editor.tsx
+++ app/Editor.tsx
@@ -11,8 +11,29 @@ import {AutoLinkNode, LinkNode} from "@lexical/link";
 import {ListNode, ListItemNode} from "@lexical/list";
 import {HeadingNode, QuoteNode} from "@lexical/rich-text";
 import React from "react";
+import LiveblocksProvider from "@liveblocks/yjs";
+import * as Y from "yjs";
+import {
+    $createParagraphNode,
+    $createTextNode,
+    $getRoot,
+    LexicalEditor,
+} from "lexical";
+import {useRoom, useSelf} from "@/liveblocks.config";
+import {CollaborationPlugin} from "@lexical/react/LexicalCollaborationPlugin";
+import {Provider} from "@lexical/yjs";
+
+function initialEditorState(editor: LexicalEditor): void {
+    const root = $getRoot();
+    const paragraph = $createParagraphNode();
+    const text = $createTextNode();
+    paragraph.append(text);
+    root.append(paragraph);
+}
 
 export const Editor: React.FC = () => {
+    const room = useRoom();
+    const userInfo = useSelf((me) => me.info);
 
     // Lexical config
     const initialConfig = {
@@ -44,6 +65,18 @@ export const Editor: React.FC = () => {
                     ErrorBoundary={LexicalErrorBoundary}
                 />
                 <MarkdownShortcutPlugin transformers={TRANSFORMERS}/>
+                <CollaborationPlugin
+                    id="yjs-plugin"
+                    providerFactory={(id, yDocMap) => {
+                        const yDoc = new Y.Doc();
+                        yDocMap.set(id, yDoc);
+                        return new LiveblocksProvider(room, yDoc) as Provider;
+                    }}
+                    initialEditorState={initialEditorState}
+                    shouldBootstrap={true}
+                    cursorColor={userInfo?.color}
+                    username={userInfo?.name}
+                />
             </LexicalComposer>
         </div>
     </div>

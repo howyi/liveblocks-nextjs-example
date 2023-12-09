ee9759cede139ae3d63aa2c71aabb20790051d7e
t-hayashi <t.hayashi.dev@gmail.com>
2023-12-09 12:52:10 +0900

    add LiveBlocksWrapper.tsx

---
+++ app/LiveBlocksWrapper.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import {RoomProvider} from "@/liveblocks.config";
+import {ClientSideSuspense} from "@liveblocks/react";
+import React from "react";
+
+export const LiveBlocksWrapper: React.FC<{ children: React.ReactNode }> = ({children}) => {
+    return (
+        <RoomProvider id="my-room" initialPresence={{}}>
+            <ClientSideSuspense fallback="Loading…">
+                {() => children}
+            </ClientSideSuspense>
+        </RoomProvider>
+    );
+}
